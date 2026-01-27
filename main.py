f2d13795ef0a0677f7fef15de02daf1c06d8d162
RyanTarnowski <ryan.tarnowski@gmail.com>
2026-01-27 12:49:51 -0700

    Setup pygame and game window

---
+++ main.py
@@ -0,0 +1,29 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    #create new instance of the game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        log_state()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("Black")
+        pygame.display.flip()
+
+
+
+
+if __name__ == "__main__":
+    main()
