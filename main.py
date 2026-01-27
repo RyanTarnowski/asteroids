a32c7d25f01750ccc9fa752395edfa3b4665eb29
RyanTarnowski <ryan.tarnowski@gmail.com>
2026-01-27 12:59:41 -0700

    Setup game clock

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     #create new instance of the game window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -21,9 +23,7 @@ def main():
         
         screen.fill("Black")
         pygame.display.flip()
-
-
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
